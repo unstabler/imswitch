99744cf2ced95b4de871e5059cc99d4621dfc03b
Gyuhwan Park <unstabler@unstabler.pl>
2020-06-19 18:48:40 +0900

    Initial Commit

---
+++ imswitch/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  imswitch
+//
+//  Created by Gyuhwan Park on 2020/06/19.
+//  Copyright © 2020 Gyuhwan Park. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
